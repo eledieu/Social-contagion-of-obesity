fb9b99eaf9fd6330c94e77ad5f0564c41929a8d8
unknown <muniche>
2014-04-07 19:08:43 +0200

    Adjacency matrix code

--- code/mssmm_adj.m
+++ code/mssmm_adj.m
@@ -1,4 +1,4 @@
-function [adj_mat,unique_source,unique_target]=mssmm_adj()
+function [adj_mat,unique_source,unique_target]=mssmm_adj(source,target,source_friendship,date_friendship_can)
     load('mssmm_data.mat')
     %% Vector of unique source
         unique_source=zeros(1,1);
@@ -36,23 +36,24 @@ function [adj_mat,unique_source,unique_target]=mssmm_adj()
             end
 
         end 
-        unique_target=unique_target(2:end)
+        unique_target=unique_target(2:end);
         length(unique_target)    
     %% Creation of the adjacency matrix
-    adj_mat=zeros(length(unique_source),length(unique_source));
-    for i=1:length(source)
-        for j=1:length(unique_source)
-            for k=1:length(unique_target)
-            if isequal(source(i),unique_source(j)) && isequal(target(i),unique_target(k))
-                if strcmp(funf_group(i),'see-peer')==1
-                    adj_mat(j,k)=2; % 2 if see peer
-                elseif strcmp(funf_group(i),'pay-peer')==1
-                    adj_mat(j,k)=1; % 1 if pay-peer
-                end
-            else
-            end
-            end
+ 
+    date=unique(date_friendship_can);
+    adj_mat=zeros(length(unique_source),length(unique_target),length(date));
+    
+    for t=1:length(date)
+        for i=1:length(source_friendship)
+            
+            % source_friendship_index returns index of person in the adjacency matrix
+            source_friendship_index=find(ismember(unique_source,source_friendship(i)));
+            target_friendship_index=find(ismember(unique_target,target_friendship(i)));
+            
+            % Adjacency matrix element value equal to weight_friendship
+            % value ranges from 0 to 7
+            adj_mat(source_friendship_index,target_friendship_index,t)=weight_friendship(i);
+            
         end
     end
-           
 end
