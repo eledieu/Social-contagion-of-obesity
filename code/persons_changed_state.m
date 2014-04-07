4bb0dde85c7f26df2780ff3d2aca38c97ece50dd
eledieu <eledieu@student.ethz.ch>
2014-04-07 08:27:34 +0200

    I only changed slightly the function, so that it is possible to identify the directionnality of the change in the output (from under to above the threshold and from above to under the threshold)

--- code/persons_changed_state.m
+++ code/persons_changed_state.m
@@ -3,7 +3,11 @@ function PersonID_x_ChangedStateAtDayX = persons_changed_state( PersonID_x_Above
 %state with respect to previous day.
 %
 % OUTPUT:: PersonID_x_ChangedStateAtDayX : Returns a unique_persons by
-% first_date_of_mesure_to_last_date_of_mesure matrix with value 1 if the person changed state with respect to previous day and 0 else.
+% first_date_of_mesure_to_last_date_of_mesure matrix with absolute value 1 if the person changed state with respect to previous day and 0 else.
+% If the person state changed from under the threshold to above the
+% threshold on the next day, the value is -1.
+% If the person state changed from above the threshold to under the
+% threshold on the next day, the value is 1.
 % INPUT::
 % PersonID_x_AboveAtExam
 
@@ -14,8 +18,7 @@ P_by_E = PersonID_x_AboveAtExam;
 
 for i= 1:m
     for j= 2:n
-        if P_by_E(i, j-1) ~= P_by_E(i, j)
-            P_by_S(i, j) = 1;
+        P_by_S(i, j) = P_by_E(i, j-1) - P_by_E(i, j) ;
     end
 end
 
