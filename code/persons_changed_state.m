5971c44ab4581ddcc6798de2296feaab32964ee5
FR4NK-W <FR4NK-W@users.noreply.github.com>
2014-04-09 11:52:02 +0200

    Update persons_changed_state.m
    Brought comment and code in sync

--- code/persons_changed_state.m
+++ code/persons_changed_state.m
@@ -3,7 +3,7 @@ function PersonID_x_ChangedStateAtDayX = persons_changed_state( PersonID_x_Above
 %state with respect to previous day.
 %
 % OUTPUT:: PersonID_x_ChangedStateAtDayX : Returns a unique_persons by
-% first_date_of_mesure_to_last_date_of_mesure matrix with absolute value 1 if the person changed state with respect to previous day and 0 else.
+% first_date_of_mesure_to_last_date_of_mesure matrix with value 1 if the person didn't changed state with respect to previous day.
 % If the person state changed from under the threshold to above the
 % threshold on the next day, the value is -1.
 % If the person state changed from above the threshold to under the
@@ -13,9 +13,6 @@ function PersonID_x_ChangedStateAtDayX = persons_changed_state( PersonID_x_Above
 
 [m,n] = size(PersonID_x_AboveAtExam);
 
-P_by_E1 = PersonID_x_AboveAtExam(:,1:(n-1)) ;
-P_by_E2 = PersonID_x_AboveAtExam(:,2:(n)) ;
-
-PersonID_x_ChangedStateAtDayX =  P_by_E1-P_by_E2;
+PersonID_x_ChangedStateAtDayX =  [[0,PersonID_x_AboveAtExam(2;n)] - [0,PersonID_x_AboveAtExam(1:n-1)]];
 
 end
