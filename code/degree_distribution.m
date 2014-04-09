98e4b36e34cba408fb8ca1b5841e38677945f184
unknown <muniche>
2014-04-09 14:26:19 +0200

    Comment added

--- code/degree_distribution.m
+++ code/degree_distribution.m
@@ -1,16 +1,17 @@
 function [P]=degree_distribution(adj_mat)
+    %% Returns the degree distribution (distribution of contact numbers) of the network at each time
     
-    date=size(adj_mat,3);
-    len_source=size(adj_mat,1);
+    date=size(adj_mat,3); % number of date
+    len_source=size(adj_mat,1); % number of persons
     
     P=zeros(len_source,date);
     
     for t=1:size(adj_mat,3)
         for k=1:len_source
-            P(k,t)=sum(adj_mat(k,:,t));
+            P(k,t)=sum(adj_mat(k,:,t)); % Number of contacts of person k at time t 
         end
         figure,
-        hist(P(:,t))
+        hist(P(:,t)); % Histogram of degree distribution
         title(['Degree distribution t=',num2str(t)],'FontSize',10,'fontweight','bold')
     end
  
