7eb673ef8c422bc84c2d71a8f702a7e64a39bb71
unknown <muniche>
2014-04-09 14:33:05 +0200

    Export to CSV adjacency matrix

--- code/export_adj_mat.m
+++ code/export_adj_mat.m
@@ -1,4 +1,6 @@
 function export_adj_mat(adj_mat)
 % Export the adjacency matrix to 
-    csvwrite('adj_matrix.csv',adj_mat)
+    variable_name=inputname(1);
+    variable_name
+    csvwrite([variable_name,'.csv'],adj_mat);
 end
