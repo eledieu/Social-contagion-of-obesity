cb583a304ec79f9fafd962305e3d775f718dba63
eledieu <eledieu@student.ethz.ch>
2014-04-09 11:11:45 +0200

    Update and rename mssmm_regression_above.m to mssmm_regression.m
    Not tested yet

---
+++ code/mssmm_regression.m
@@ -0,0 +1,34 @@
+function changes =mssmm_regression(persons_changed_states, count_contacts_above, number_contacts, under_to_above)
+ % Compute the number of changes from under to above (or under) the threshold value
+ % with respect to the number of contacts under (or above) threshold at the time step
+ % of the change
+ 
+ %OUTPUT :: A column vector of the number of changes. The index of the
+ %column vector corresponds to the number of contacts in the new state +1
+ 
+ %INPUT ::
+ % persons_changed_state : output of the function persons_changed_state
+ % (Returns a unique_persons by
+ % first_date_of_mesure_to_last_date_of_mesure matrix with absolute value 1
+ % if the person changed state with respect to previous day and 0 else.)
+ % count_matrix_above : output of the count_matrix function
+ % number_contacts : column vector with the total number of contacts of
+ % each person
+ % under_to_above : boolean value. if TRUE, then changes from under_to_above ; else
+ % then compute the changes from state above to state under threshold
+ 
+ n = size(count_contacts_above, 2);
+ 
+ pcs = -persons_changed_state;
+ contacts_new_state = count_contacts_above(:,2:n);
+ 
+ if NOT(under_to_above)
+     pcs = persons_changed_state;
+     contacts_new_state = repmat(number_contacts, 1, n-1)-contacts_new_state;
+ end
+
+ changes_with_percentage = tabulate(contacts_new_state(pcs>0));
+ changes = changes_with_percentage(:,1:2)
+ 
+    
+end
