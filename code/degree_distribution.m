f7bf264d34dfb3e75cd1a31bddf52fbba9dc8be5
unknown <muniche>
2014-04-12 19:42:10 +0200

    Code commented

--- code/degree_distribution.m
+++ code/degree_distribution.m
@@ -1,14 +1,21 @@
-function [P]=degree_distribution(adj_mat)
+function [P]=degree_distribution(adj_mat_bin)
     %% Returns the degree distribution (distribution of contact numbers) of the network at each time
-    
-    date=size(adj_mat,3); % number of date
-    len_source=size(adj_mat,1); % number of persons
+    % Input:
+            % Binary adjacency matrix: 1 if person are close friends and 0
+            % otherwise
+    % Ouput:
+            % P matrix with the number of contacts of each person at the
+            % each time
+            % The plots displays the degree distribution histogram
+            
+    date=size(adj_mat_bin,3); % number of date
+    len_source=size(adj_mat_bin,1); % number of persons
     
     P=zeros(len_source,date);
     
-    for t=1:size(adj_mat,3)
+    for t=1:date
         for k=1:len_source
-            P(k,t)=sum(adj_mat(k,:,t)); % Number of contacts of person k at time t 
+            P(k,t)=sum(adj_mat_bin(k,:,t)); % Number of close friends of person k at time t 
         end
         figure,
         hist(P(:,t)); % Histogram of degree distribution
