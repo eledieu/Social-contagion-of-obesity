229ceb192b9d65dc19988597b85a37babb92f43f
unknown <muniche>
2014-05-03 17:18:06 +0200

    update

--- code/plot_graph_report.m
+++ code/plot_graph_report.m
@@ -5,24 +5,24 @@ function [] = plot_graph_report(number_of_contacts,avg_degree,knn)
     for t=1:size(avg_degree,1)
         total_contacts=sum(number_of_contacts(:,t));
         figure;
-        bar(1:max(number_of_contacts(:,t)),1/total_contacts*histc(number_of_contacts(:,t),1:max(number_of_contacts(:,t))),'histc')% Histogram of degree distribution
-        title(['Close Friends Distribution t=',num2str(t)],'FontSize',10,'fontweight','bold')
+        bar(1:max(number_of_contacts(:,t)),1/total_contacts*histc(number_of_contacts(:,t),1:max(number_of_contacts(:,t))),'histc');% Histogram of degree distribution
+        title(['Close Friends Distribution t=',num2str(t)],'FontSize',10,'fontweight','bold');
         
         figure;       
-        hist(number_of_contacts(:,t))% Histogram of degree distribution
-        xlabel('Number of Close Friends')
-        ylabel('Close Friends Frequency')
-        title(['Close Friends Distribution t=',num2str(t)],'FontSize',10,'fontweight','bold')
+        hist(number_of_contacts(:,t));% Histogram of degree distribution
+        xlabel('Number of Close Friends');
+        ylabel('Close Friends Frequency');
+        title(['Close Friends Distribution t=',num2str(t)],'FontSize',10,'fontweight','bold');
     end
 %% Network assortativity
     for t=1:size(knn,2)
-        axis=1:size(knn,1)
+        axis=1:size(knn,1);
             figure;
-            knn(knn==0)=nan
-            plot(axis(1:45),knn(1:45,t),'*')
-            xlabel('Average Degree k')
-            ylabel('Average Nearest Neighbor Degree knn(k)')            
-            title(['Network Assortativity t=',num2str(t)],'FontSize',10,'fontweight','bold')
+            knn(knn==0)=nan;
+            plot(axis(1:45),knn(1:45,t),'*');
+            xlabel('Average Degree k');
+            ylabel('Average Nearest Neighbor Degree knn(k)');            
+            title(['Network Assortativity t=',num2str(t)],'FontSize',10,'fontweight','bold');
     end
 end
 
