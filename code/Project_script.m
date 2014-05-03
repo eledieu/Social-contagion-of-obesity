9c5ee4950f2913e7130426a65799a22017eb498a
unknown <muniche>
2014-05-03 17:15:48 +0200

    Modified one name of input

--- code/Project_script.m
+++ code/Project_script.m
@@ -1,7 +1,7 @@
 function Project_script(studied_variable,threshold)
     load('mssmm_data.mat')
     [adj_mat,adj_mat_bin,unique_source,unique_target]=mssmm_adj(source,target,source_friendship,date_friendship_can)
-    [P]=degree_distribution(adj_mat_bin)
+    [contacts,avg_degree]=degree_distribution(adj_mat_bin)
     PersonID_x_AboveAtExam = persons_above( participantID_weight,studied_variable, date_weigth_can,threshold)
     PersonID_x_ChangedStateAtDayX = persons_changed_state( PersonID_x_AboveAtExam )
     [number_of_contacts] = count_obese_contacts(PersonID_x_AboveAtExam, adj_mat_bin)
