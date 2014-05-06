33d76ddfcfb3246b4dd3a645046b23cb1250ce4f
unknown <muniche>
2014-05-06 18:14:28 +0200

    Degree distribution fit

--- code/power_law_fit.m
+++ code/power_law_fit.m
@@ -1,4 +1,4 @@
-function [distribution_degree_h,distribution_degree_pval] = power_law_fit(contacts)
+function [distribution_degree_coeff] = power_law_fit(contacts)
 % Returns a Power law fit of the network distribution degree 
 
 % Input:
@@ -12,6 +12,10 @@ function [distribution_degree_h,distribution_degree_pval] = power_law_fit(contac
 % distribution_degree_pval=p-value p of the hypothesis test
 
 %% Distribution degree power law fit
+distribution_degree_coeff=zeros(size(contacts,2),2);
+distribution_degree_h=zeros(size(contacts,2),1);
+distribution_degree_pval=zeros(size(contacts,2),1);
+
 for t=1:size(contacts,2)
     total_contacts=sum(contacts(:,t));
     
@@ -21,18 +25,22 @@ for t=1:size(contacts,2)
     
     % Curve fitting tool
     log_centers=log(centers);
-    log_elements=log(1/total_contacts*nelements);
-    
-    fit_power_law=fitlm(log_elements,log_centers);
-    fit_coeff=double(fit_power_law.Coefficients)
+    log_nelements=log(nelements);
+    fit_power_law=fitlm(log_centers,log_nelements,'linear');
+    fit_coeff=double(fit_power_law.Coefficients);
     
     % alpha*x^(beta)
-    alpha=fit_coeff(1,1);
-    beta=fit_coeff(2,1);
+    distribution_degree_coeff(t,1)=fit_coeff(1,1);
+    distribution_degree_coeff(t,2)=fit_coeff(2,1);
+    
+    lnalpha=distribution_degree_coeff(t,1);
+    beta=distribution_degree_coeff(t,2);
     
     % Kolomogorov Smirnov test
-    [distribution_degree_h,distribution_degree_pval]=kstest(nelements,'Alpha',0.05,'CDF',[transpose(nelements),alpha*transpose(nelements).^beta]);
-    
+    % [h,pval]=kstest(nelements,'Alpha',0.05,'CDF',[transpose(centers),exp(lnalpha)*transpose(centers).^beta]);
+    % distribution_degree_h(t,1)=h;
+    % distribution_degree_pval(t,1)=pval;
+     
 end
 
 end
