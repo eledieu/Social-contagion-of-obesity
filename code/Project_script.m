f23314caa9bf501f097c74f437a6986d7f109751
FR4NK-W <wirzf@student.ethz.ch>
2014-05-06 10:17:38 +0200

    Updated project script

--- code/Project_script.m
+++ code/Project_script.m
@@ -1,12 +1,12 @@
 function Project_script(studied_variable,threshold)
     load('mssmm_data.mat')
-    [adj_mat,adj_mat_bin,unique_source,unique_target]=mssmm_adj(source,target,source_friendship,date_friendship_can)
+    [adj_mat,adj_mat_bin,unique_source,unique_target]=mssmm_adj(participantID_weight,participantID_weight,source_friendship,date_friendship_can)
     [contacts,avg_degree]=degree_distribution(adj_mat_bin)
     PersonID_x_AboveAtExam = persons_above( participantID_weight,studied_variable, date_weigth_can,threshold)
     PersonID_x_ChangedStateAtDayX = persons_changed_state( PersonID_x_AboveAtExam )
     [number_of_contacts] = count_obese_contacts(PersonID_x_AboveAtExam, adj_mat_bin)
-    change_under=mssmm_regression(PersonID_x_ChangedStateAtDayX(1:90,:), number_of_contacts, sum(adj_mat_bin(:,:,1)),true)
-    change_above=mssmm_regression(PersonID_x_ChangedStateAtDayX(1:90,:), number_of_contacts, sum(adj_mat_bin(:,:,1)),false)
+    change_under=mssmm_regression(PersonID_x_ChangedStateAtDayX, number_of_contacts, sum(adj_mat_bin(:,:,1)),true)
+    change_above=mssmm_regression(PersonID_x_ChangedStateAtDayX, number_of_contacts, sum(adj_mat_bin(:,:,1)),false)
     [above b g a] =mssmm_regression_results(change_above, change_under)
 end
 
