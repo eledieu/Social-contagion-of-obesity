b9210a58824b328244ad3fc7eefee2fddaf8f857
unknown <muniche>
2014-05-08 00:07:05 +0200

    Update BMI distribution code

--- code/BMI_distribution.m
+++ code/BMI_distribution.m
@@ -1,10 +1,20 @@
-function [ output_args ] = BMI_distribution( BMI )
+function [] = BMI_distribution(BMI,participantID_weight)
 % Returns the distribution of the BMI
 %   Input: %% BMI
 %
 %   Output: %% distribution of BMI
-
-    hist(BMI)
+    
+    unique_participantID_weight=unique(participantID_weight);
+    
+    unique_BMI=zeros(length(unique_participantID_weight),1);
+    
+    for i=1:length(unique_participantID_weight)
+        index_unique=find(ismember(participantID_weight,unique_participantID_weight(i)));
+        Index_unique=index_unique(1);
+        unique_BMI(i)=BMI(Index_unique);
+    end
+    axis=1:50;
+    histfit(unique_BMI,50);
 
 end
 
