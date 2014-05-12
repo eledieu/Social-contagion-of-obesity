0fd3cf0a8c616ef17fa3dcb47548f6d463428996
FR4NK-W <FR4NK-W@users.noreply.github.com>
2014-05-12 19:24:51 +0200

    Update Project_script.m
    Removed duplicate plot

--- code/Project_script.m
+++ code/Project_script.m
@@ -1,5 +1,4 @@
-%function Project_script(studied_variable,threshold)
-	load('DATA.mat')
+    load('DATA.mat')
     prompt = 'What variable do you want to study? Type BMI or body_fat  ';
     studied_variable = input(prompt);
     prompt = 'Choose a meaningful threshold for the variable:';
@@ -10,14 +9,12 @@
     [contacts,avg_degree] = degree_distribution(adj_mat_bin);
     disp(avg_degree);
     % Is the ego above the threshold at the exam:
-    PersonID_x_AboveAtExam = persons_above( participantID_weight,studied_variable, date_weigth_can,threshold);
-	%Plots the evolution of the number of person above figure
+    PersonID_x_AboveAtExam = persons_above_interpolated( participantID_weight,studied_variable, date_weigth_can,threshold);
+	%Plots the evolution of the number of person above
+    figure(1)
     plot(sum(PersonID_x_AboveAtExam)/size(PersonID_x_AboveAtExam,1))
 	% When did a transition from one state to the next happen and which way:
     PersonID_x_ChangedStateAtDayX = persons_changed_state( PersonID_x_AboveAtExam );
-    %Plots the evolution of the number of person above
-    figure
-    plot(sum(PersonID_x_AboveAtExam)/size(PersonID_x_AboveAtExam,1))
     % Number of contacts above the threshold at the exam:
     [number_of_contacts] = count_obese_contacts(PersonID_x_AboveAtExam, adj_mat_bin);
     % Percentage of total transitions happening given a count of contacts
@@ -27,7 +24,7 @@
     % regression on the parameters
     [above b g a] = mssmm_regression_results(change_above, change_under)
     % plot regression results
-    figure(1);
+    figure(2);
     plot(change_above(:,1),change_above(:,2))
     hold on;
     x = change_above(:,1);
@@ -35,9 +32,8 @@
     plot(x,y*100,'o');
     hold off;
     initial_infected = sum(PersonID_x_AboveAtExam(:,1)) * (100 / size(PersonID_x_AboveAtExam,1));
-    figure(2);
+    figure(3);
     [susceptible,infected] = simulation(a,b,g, initial_infected, size(PersonID_x_AboveAtExam,2), 0.01);
-%end
 
 % First test: studied_variable=BMI, threshold=26: slight correlation,
 % ~stable state at 75% infected
