cfbc6d4ac42ae7c4f9e81a12ddeb3b5cf3560c83
FR4NK-W <wirzf@student.ethz.ch>
2014-05-13 17:49:04 +0200

    Cofounding removed input, changed loaded file

--- code/obesity_confounding.m
+++ code/obesity_confounding.m
@@ -1,10 +1,10 @@
-function [ output_args ] = obesity_confounding(MEDHHINC10,PCT_OBESE_ADULTS10)
+function [ output_args ] = obesity_confounding()
 % Returns the regression of the US obesity by county against the median
 % household income in 2010
 % Source:http://www.ers.usda.gov/data-products/food-environment-atlas/data-access-and-documentation-downloads.aspx#.U3E4pSg-Vo4
 
     % Load input file
-    load('confounding_input.mat')
+    load('DATA.mat')
     % Regression
     fitlm(MEDHHINC10/1000,PCT_OBESE_ADULTS10)
 
